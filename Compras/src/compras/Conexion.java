e95945c5c35d2cd98080fcfb2598e342d2ebe829
joseantonioq <79658038+joseantonioq@users.noreply.github.com>
2021-03-04 11:59:09 -0600

    Add files via upload

---
+++ Compras/src/compras/Conexion.java
@@ -0,0 +1,28 @@
+
+package compras;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+public class Conexion 
+{
+    public static Connection getConexion()     
+       {
+        String conexion="jdbc:sqlserver://localhost:1433;"
+                +"database=Compras;"
+                +"user=sa;"
+                +"password=toni;"
+                +"loginTimeout=30;";
+        try
+            {
+                Connection con=DriverManager.getConnection(conexion);
+                return con;
+            }catch(SQLException ex){
+                System.out.println(ex.toString());
+                return null;
+            }
+                
+       }     
+}
